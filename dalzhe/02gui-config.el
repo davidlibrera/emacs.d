ace00abe511d0e6b0fd87ce4892ff8d7ffa4717f
David Librera <d.librera@cantierecreativo.net>
2015-04-07 21:22:12 +0200

    added config for ui

---
+++ dalzhe/02gui-config.el
@@ -0,0 +1,36 @@
+;; Emacs graphic ui settings
+(setq inhibit-startup-message t)                            ; Disable startup screen
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))        ; No scroll bar
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))            ; No tool bar
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))            ; No menu bar
+(set-default 'cursor-type 'bar)                             ; Bar Cursor
+(blink-cursor-mode -1)                                      ; the blinking cursor is nothing, but an annoyance
+
+;; Nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
+
+;; Caption is pathname/temp buffer name
+(setq frame-title-format
+      '(buffer-file-name "%f" (dired-directory dired-directory "%b")))
+
+;; Dont't wrap
+(set-default 'truncate-lines t)
+
+;;Position indicator
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; Line number on side
+(setq linum-format "%3d ")
+(global-linum-mode)
+
+(if (fboundp 'fringe-mode) (fringe-mode 0))
+
+(load "highlight-indentation")
+(set-face-background 'highlight-indentation-face "#292929")
+(set-face-background 'highlight-indentation-current-column-face "#333333")
+
+(load-theme 'molokai t)
--- init.el
+++ init.el
@@ -1,7 +1,10 @@
-(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
-(cask-initialize)
-(require 'pallet)
+;;(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
+;;(cask-initialize)
+;;(require 'pallet)
 
-(add-to-list 'load-path "~/.emacs.d/dalzhe")
+;;(add-to-list 'load-path "~/.emacs.d/dalzhe")
 
-(load "00global-setup.el")
+;;(load "00global-setup.el")
+;;(load "01flx-ido.el")
+;;(load "02gui-config.el")
+stocazzo
