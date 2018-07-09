862379e4d33799cf3d32fb19da1f9edbea950e45
David Librera <davidlibrera@gmail.com>
2018-07-09 11:33:09 +0200

    Js-mode for jsx

--- elisp/modes/js-mode.el
+++ elisp/modes/js-mode.el
@@ -1,4 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (add-to-list 'auto-mode-alist '("\\.es6$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js-mode))
 
 (setq js-indent-level 2)
