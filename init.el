5852224e10b3f8cf153db14f2d6f4fbb82253125
David Librera <davidlibrera@gmail.com>
2018-08-01 09:39:23 +0200

    Show installed packages in list

--- init.el
+++ init.el
@@ -37,3 +37,17 @@
 
 (set-face-attribute 'default nil
                     :family "Ubuntu Mono" :height 110 :weight 'normal)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flymd markdown-mode+ yasnippet yaml-mode web-mode visual-regexp smartparens slim-mode sass-mode ruby-tools ruby-hash-syntax rspec-mode rbenv projectile pallet neotree move-text molokai-theme markdown-preview-mode markdown-preview-eww magit indent-guide helm flx-ido expand-region evil enh-ruby-mode emojify elixir-mode coffee-mode auto-complete ag ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
