5fc33b68f108a09e9c35625e271ededd9946dcdf
David Librera <davidlibrera@gmail.com>
2021-05-11 23:55:05 +0200

    Configure ruby-mode

---
+++ elisp/modes/ruby-mode.el
@@ -0,0 +1,22 @@
+(global-set-key (kbd "C-.") 'ruby-toggle-hash-syntax)
+(add-hook 'enh-ruby-mode-hook
+    (lambda ()
+      (ruby-tools-mode)
+      ))
+
+(setq rspec-use-rake-when-possible nil)
+(eval-after-load 'rspec-mode
+  '(rspec-install-snippets))
+(add-hook 'rspec-compilation-mode-hook
+          (lambda ()
+            (setq truncate-lines nil)
+            ))
+
+(setq auto-mode-alist (cons '("Rakefile" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Guardfile" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Gemfile" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Cheffile" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.gemspec" . enh-ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.builder" . enh-ruby-mode) auto-mode-alist))
--- init.el
+++ init.el
@@ -21,6 +21,7 @@
 (load "modes/slim-mode")
 (load "modes/web-mode")
 (load "modes/js-mode")
+(load "modes/ruby-mode")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -28,7 +29,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(helm sass-mode indent-guide slim-mode smart-tab undo-tree elixir-mode evil molokai-theme yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff)))
+   '(rspec-mode helm sass-mode indent-guide slim-mode smart-tab undo-tree elixir-mode evil molokai-theme yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
