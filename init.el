73636ca6ddecb1c6009b6fe4fbd66121864dcba6
David Librera <davidlibrera@gmail.com>
2022-05-06 03:09:17 +0200

    Update cask

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
+
 (setq warning-suppress-log-types '((package reinitialization)))
-(require 'cask "/opt/homebrew/Cellar/cask/0.8.7/cask.el")
+(require 'cask "/opt/homebrew/Cellar/cask/0.8.8/cask.el")
 (cask-initialize)
 (require 'pallet)
 
