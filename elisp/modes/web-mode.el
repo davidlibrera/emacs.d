550e51034fda1dd7df7cedfaf4a897abd628874b
David Librera <davidlibrera@gmail.com>
2022-05-06 03:09:04 +0200

    Expand web-mode extensions

--- elisp/modes/web-mode.el
+++ elisp/modes/web-mode.el
@@ -1,4 +1,9 @@
-(add-to-list 'auto-mode-alist '("\\.(jsx|css|html|eex|leex)$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.leex$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.sface$" . web-mode))
 
 (defun web-mode-init ()
   "Init hooks for web mode"
