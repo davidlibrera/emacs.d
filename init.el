7b16c93a8450cb9ff13485762b7e36acbcf1d975
David Librera <davidlibrera@gmail.com>
2022-05-06 03:09:32 +0200

    Add solarized theme

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ag all-the-icons neotree rspec-mode helm sass-mode indent-guide slim-mode smart-tab undo-tree elixir-mode evil molokai-theme yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff)))
+   '(solarized-theme ag all-the-icons neotree rspec-mode helm sass-mode indent-guide slim-mode smart-tab undo-tree elixir-mode evil molokai-theme yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
